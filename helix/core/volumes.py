5cc71922f743e5665338907c54496476cacbe8c6
Ragnor Comerford <hello@ragnor.co>
2024-05-22 18:10:13 +0100

    Add "results" volume

--- helix/core/volumes.py
+++ helix/core/volumes.py
@@ -4,3 +4,5 @@ from modal import Volume
 model_weights = Volume.from_name(
     "model-weights", create_if_missing=True)
 databases = Volume.from_name("databases", create_if_missing=True)
+
+results = Volume.from_name("results", create_if_missing=True)
