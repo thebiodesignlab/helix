cf2ab3b0055e767c338e365c09a5515763c2e83a
Ragnor Comerford <hello@ragnor.co>
2024-05-28 10:41:35 +0100

    Add CloudBucketMount for rosetta and update mmseqs_databases volume

--- helix/core/volumes.py
+++ helix/core/volumes.py
@@ -1,8 +1,12 @@
-from modal import Volume
+from modal import Volume, CloudBucketMount, Secret
 
 
 model_weights = Volume.from_name(
     "model-weights", create_if_missing=True)
-databases = Volume.from_name("databases", create_if_missing=True)
+
+rosetta = CloudBucketMount(
+    "helix", secret=Secret.from_name("cloudflare-r2-secrets"), bucket_endpoint_url="https://b6e2e34967f8dc08d00fe1b17a2fd681.r2.cloudflarestorage.com")
+
+mmseqs_databases = Volume.from_name("mmseqs-databases", create_if_missing=True)
 
 results = Volume.from_name("results", create_if_missing=True)
